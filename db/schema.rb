97ac5471fd107800c5275f28c9e0102f5f9f64e1
Garrett Martin <garrettqmartin8@optimum.net>
2013-11-18 20:41:58 -0600

    add user id to workouts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118160846) do
+ActiveRecord::Schema.define(version: 20131119024047) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131118160846) do
     t.integer  "total_reps"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
