34ad3fa8a1ab2f6ac9e9fbb5c092c541956d14c8
Garrett Martin <garrettqmartin8@optimum.net>
2013-11-30 17:40:23 -0600

    Ajaxify user progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user.password_confirmation = params[:password_confirmation]
     
     if @user.save
-      session[:user_id] = @user.id
+      session[:user_id] = @user.id      
       redirect_to workouts_url
     else
       # TODO add flash error
@@ -21,6 +21,9 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_id(session[:user_id])
+    respond_to do |format|
+      format.js
+    end
   end
   
   
